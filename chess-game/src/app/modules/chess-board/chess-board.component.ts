9cdb8c0fbc714d1506d9a559246773d0dd158de6
Pedro Ibanez <pibanezg@miumg.com.gt>
2024-10-06 18:02:50 -0600

    finished building the display of chessboard and added pieces images and positions

--- chess-game/src/app/modules/chess-board/chess-board.component.ts
+++ chess-game/src/app/modules/chess-board/chess-board.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ChessBoard } from 'src/app/chess-logic/chess-board';
+import { pieceImagePaths } from 'src/app/chess-logic/models';
 import { Color, FENChar } from 'src/app/chess-logic/pieces/models';
 
 @Component({
@@ -8,7 +9,13 @@ import { Color, FENChar } from 'src/app/chess-logic/pieces/models';
   styleUrls: ['./chess-board.component.css']
 })
 export class ChessBoardComponent {
+  public pieceImagePaths = pieceImagePaths;
+
   private chessBoard = new ChessBoard();
   public chessBoardView: (FENChar | null)[][] = this.chessBoard.chessBoardView;
   public get playerColor(): Color {return this.chessBoard.playerColor; };
+
+  public isSquareDark(x: number, y: number): boolean{
+    return ChessBoard.isSquareDark(x, y);
+  }
 }
