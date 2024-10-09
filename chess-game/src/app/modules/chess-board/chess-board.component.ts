8be73ee784bb8fe09044dec40e2da35641fd6255
Pedro Ibanez <pibanezg@miumg.com.gt>
2024-10-08 21:36:35 -0600

    adding handling of event when a squere is selected twice

--- chess-game/src/app/modules/chess-board/chess-board.component.ts
+++ chess-game/src/app/modules/chess-board/chess-board.component.ts
@@ -34,10 +34,19 @@ export class ChessBoardComponent {
     return this.pieceSafeSquares.some(coords => coords.x === x && coords.y === y);
   }
 
+  private unmarkingPreviouslySelectedAndSafeSquares(): void{
+    this.selectedSquare = {piece:null};
+    this.pieceSafeSquares = [];
+  }
+
   public selectingPiece(x: number, y: number): void{
     const piece: FENChar | null = this.chessBoardView[x][y];
     if(!piece) return;
     if(this.isWrongPieceSelected(piece)) return;
+    
+    const isSameSquareClicked: boolean = !!this.selectedSquare.piece && this.selectedSquare.x === x && this.selectedSquare.y === y;
+    this.unmarkingPreviouslySelectedAndSafeSquares();
+    if(isSameSquareClicked) return;
 
     this.selectedSquare = {piece, x, y};
     this.pieceSafeSquares = this.safeSquares.get(x + "," + y) || [];
@@ -46,10 +55,11 @@ export class ChessBoardComponent {
   private placingPiece(newX: number, newY: number): void{
     if(!this.selectedSquare.piece) return;
     if(!this.isSquareSafeForSelectedPiece(newX, newY)) return;
-
+    
     const {x: prevX, y: prevY} = this.selectedSquare;
     this.chessBoard.move(prevX, prevY, newX, newY);
     this.chessBoardView = this.chessBoard.chessBoardView;
+    this.unmarkingPreviouslySelectedAndSafeSquares();
   }
 
   public move(x: number, y: number): void{
